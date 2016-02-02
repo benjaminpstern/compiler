0697ad4ec915c332b5e2fddc812e8c64f95f8d0b
benjaminpstern <bstern@oberlin.edu>
2016-02-02 04:35:06 -0500

    added gitignore

--- test/sampletest.cpp
+++ test/sampletest.cpp
@@ -8,6 +8,13 @@ TEST(SampleTest, TestSample) {
     s.variable = 5;
     ASSERT_FLOAT_EQ(s.func(4), 9.1);
 }
+
+TEST(SampleTest, TestSample2) {
+    sample s;
+    s.variable = 2;
+    ASSERT_FLOAT_EQ(s.func(4), 6.1);
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
