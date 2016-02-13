003ed6e0ff4fe001eadd995b3abf05e3b70b55c3
Ben Stern <bstern@occs.cs.oberlin.edu>
2016-02-12 20:30:49 -0500

    added readme and changed main

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,17 @@
 #include <iostream>
 #endif
 
+#ifndef IOMANIP
+#include <iomanip>
+#endif
+
 #ifndef TOKENIZER
 #include "tokenizer.h"
 #endif
 
 using std::cout;
 using std::endl;
+using std::setw;
 int main(int argc, char** argv) {
     if (argc < 2) {
         cout << "Please enter a filename" << endl;
@@ -15,7 +20,10 @@ int main(int argc, char** argv) {
     }
     tokenizer t(argv[1]);
     while (t.has_next_token()) {
-        cout << t.next_token().get_str() << endl;
+        token tok = t.next_token();
+        cout << "token string:" << setw(15) << tok.get_str() << setw(15)
+            << "token type:" << setw(20) << tok.get_type() << setw(15) 
+            << "line num:" << setw(4) << tok.get_line_num() << endl;
     }
     return 0;
 }
